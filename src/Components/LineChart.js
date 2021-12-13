b10d610c29865e24665d50f8bf76055f4e1a0bba
beranrdn38 <erisboxx@gmail.com>
2021-12-13 14:49:51 -0800

    update 1.7.0

--- src/Components/LineChart.js
+++ src/Components/LineChart.js
@@ -75,7 +75,7 @@ export default function LineChart({ index }) {
     const resData = [];
 
     axios
-      .get(`http://localhost:8000/api/transactions/sum?type=${types[index]}`)
+      .get(`http://0.0.0.0:8000/api/transactions/sum?type=${types[index]}`)
       .then((response) => {
 
         labels.map((month,idx) => {
